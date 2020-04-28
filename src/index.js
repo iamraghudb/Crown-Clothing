0897bead0d7758e68e610b3a1b12eabd5fba60cc
iamraghudb <54179651+iamraghudb@users.noreply.github.com>
2020-04-29 01:05:23 +0530

    added redux to directory component

--- src/index.js
+++ src/index.js
@@ -4,13 +4,17 @@ import { BrowserRouter } from "react-router-dom";
 import { Provider } from "react-redux";
 import "./index.css";
 import App from "./App";
-import store from "./redux/store";
+import { store } from "./redux/store";
+import { persistor } from "./redux/store";
+import { PersistGate } from "redux-persist/integration/react";
 
 ReactDOM.render(
   <Provider store={store}>
     <React.StrictMode>
       <BrowserRouter>
-        <App />
+        <PersistGate persistor={persistor}>
+          <App />
+        </PersistGate>
       </BrowserRouter>
     </React.StrictMode>
   </Provider>,
