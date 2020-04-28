e0e5549bbfee37aaeb5cbb12324ff50aebb135ca
iamraghudb <54179651+iamraghudb@users.noreply.github.com>
2020-04-29 01:34:04 +0530

    added redux to COLLECTION component

--- src/components/shop.component.jsx
+++ src/components/shop.component.jsx
@@ -1,21 +1,19 @@
 import React, { Component } from "react";
-import SHOP_DATA from "./shop.data";
 import CollectionPreview from "./collection-preview.component";
-class ShopPage extends Component {
-  constructor(props) {
-    super(props);
-    this.state = { collections: SHOP_DATA };
-  }
-  render() {
-    const { collections } = this.state;
-    return (
-      <div className="shop-page">
-        {collections.map(({ id, ...otherCollectionProps }) => (
-          <CollectionPreview key={id} {...otherCollectionProps} />
-        ))}
-      </div>
-    );
-  }
-}
+import { selectCollections } from "./../redux/shop/shop.selectors";
+import { connect } from "react-redux";
+import { createStructuredSelector } from "reselect";
 
-export default ShopPage;
+const ShopPage = ({ collections }) => (
+  <div className="shop-page">
+    {collections.map(({ id, ...otherCollectionProps }) => (
+      <CollectionPreview key={id} {...otherCollectionProps} />
+    ))}
+  </div>
+);
+
+const mapStateToProps = createStructuredSelector({
+  collections: selectCollections,
+});
+
+export default connect(mapStateToProps)(ShopPage);
---
+++ src/redux/shop/shop.selectors.js
@@ -0,0 +1,8 @@
+import { createStructuredSelector, createSelector } from "reselect";
+
+const selectShop = (state) => state.shop;
+
+export const selectCollections = createSelector(
+  [selectShop],
+  (shop) => shop.collections
+);
