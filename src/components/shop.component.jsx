fbaabd4cb5470fcaec66b00f7cb443cfa9727323
iamraghudb <54179651+iamraghudb@users.noreply.github.com>
2020-05-04 16:57:52 +0530

    added nested routing

--- src/components/shop.component.jsx
+++ src/components/shop.component.jsx
@@ -1,19 +1,12 @@
-import React, { Component } from "react";
-import CollectionPreview from "./collection-preview.component";
-import { selectCollections } from "./../redux/shop/shop.selectors";
-import { connect } from "react-redux";
-import { createStructuredSelector } from "reselect";
-
-const ShopPage = ({ collections }) => (
+import React from "react";
+import { Route } from "react-router-dom";
+import CollectionsOverview from "./collections-overview.component";
+import Collection from "./collection.component";
+const ShopPage = ({ match }) => (
   <div className="shop-page">
-    {collections.map(({ id, ...otherCollectionProps }) => (
-      <CollectionPreview key={id} {...otherCollectionProps} />
-    ))}
+    <Route exact path={`${match.path}`} component={CollectionsOverview} />
+    <Route path={`${match.path}/:collectionId`} component={Collection} />
   </div>
 );
 
-const mapStateToProps = createStructuredSelector({
-  collections: selectCollections,
-});
-
-export default connect(mapStateToProps)(ShopPage);
+export default ShopPage;
--- src/redux/shop/shop.selectors.js
+++ src/redux/shop/shop.selectors.js
@@ -1,4 +1,12 @@
-import { createStructuredSelector, createSelector } from "reselect";
+import { createSelector } from "reselect";
+
+// const COLLECTION_ID_MAP = {
+//   hats: 1,
+//   sneakers: 2,
+//   jackets: 3,
+//   womens: 4,
+//   mens: 5,
+// };
 
 const selectShop = (state) => state.shop;
 
@@ -6,3 +14,14 @@ export const selectCollections = createSelector(
   [selectShop],
   (shop) => shop.collections
 );
+
+export const selectCollectionsForPreview = createSelector(
+  [selectCollections],
+  (collections) => Object.keys(collections).map((key) => collections[key])
+);
+
+export const selectCollection = (collectionUrlParam) =>
+  createSelector(
+    [selectCollections],
+    (collections) => collections[collectionUrlParam]
+  );
