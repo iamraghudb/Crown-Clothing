fc0ce89213f13f25c51369422f5391e997da7ca9
iamraghudb <54179651+iamraghudb@users.noreply.github.com>
2020-11-02 18:32:42 +0530

    added link to titles

--- src/components/collection-preview.component.jsx
+++ src/components/collection-preview.component.jsx
@@ -1,21 +1,47 @@
+// import React from "react";
+// import "./collection-preview.styles.scss";
+// import CollectionItem from "./collection-item.component";
+// // import Collection from "./collection.component";
+
+// const CollectionPreview = ({ title, items }) => {
+//   return (
+//     <div className="collection-preview">
+//       <h1 className="title">{title.toUpperCase()}</h1>
+//       <div className="preview">
+//         {items
+//           .filter((item, idx) => idx < 4)
+//           .map((item) => (
+//             <CollectionItem key={item.id} item={item} />
+//           ))}
+//       </div>
+//     </div>
+//   );
+// };
+
+// export default CollectionPreview;
+
 import React from "react";
-import "./collection-preview.styles.scss";
+import { withRouter } from "react-router-dom";
 import CollectionItem from "./collection-item.component";
-// import Collection from "./collection.component";
 
-const CollectionPreview = ({ title, items }) => {
-  return (
-    <div className="collection-preview">
-      <h1 className="title">{title.toUpperCase()}</h1>
-      <div className="preview">
-        {items
-          .filter((item, idx) => idx < 4)
-          .map((item) => (
-            <CollectionItem key={item.id} item={item} />
-          ))}
-      </div>
+import "./collection-preview.styles.scss";
+
+const CollectionPreview = ({ title, items, history, match }) => (
+  <div className="collection-preview">
+    <h1
+      className="title"
+      onClick={() => history.push(`${match.url}/${title.toLowerCase()}`)}
+    >
+      {title.toUpperCase()}
+    </h1>
+    <div className="preview">
+      {items
+        .filter((item, idx) => idx < 4)
+        .map((item) => (
+          <CollectionItem key={item.id} item={item} />
+        ))}
     </div>
-  );
-};
+  </div>
+);
 
-export default CollectionPreview;
+export default withRouter(CollectionPreview);
